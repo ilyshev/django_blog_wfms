69a19816d9b0a0843202dba716c955a80747f910
ilyshev <dilyshev@mac.com>
2020-09-23 17:55:43 -0300

    Showing posts on the main page

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -1,4 +1,19 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+
+from .models import *
+
+
+class Home(ListView):
+    model = Post
+    template_name = 'blog/index.html'
+    context_object_name = 'posts'
+    paginate_by = 2
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['title'] = 'Classic blog design'
+        return context
 
 
 def index(request):
@@ -6,4 +21,8 @@ def index(request):
 
 
 def get_category(request, slug):
+    return render(request, 'blog/category.html')
+
+
+def get_post(request, slug):
     return render(request, 'blog/category.html')
